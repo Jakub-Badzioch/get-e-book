cbed37a9060061be2b7e5579e87ac6f18c652b7e
Jakub-Badzioch <jakub.badzioch.main@gmail.com>
2022-01-03 13:11:29 +0100

    init

---
+++ src/main/java/com/getabook/GetABookApplication.java
@@ -0,0 +1,13 @@
+package com.getabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetABookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetABookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/getabook/GetABookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetABookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
