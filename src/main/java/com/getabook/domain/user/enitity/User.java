a1f1f602a1b904d83c18f9a94b026045590c146a
Jakub-Badzioch <jakub.badzioch.main@gmail.com>
2022-01-05 16:27:14 +0100

    User entity

---
+++ src/main/java/com/getabook/domain/user/enitity/User.java
@@ -0,0 +1,31 @@
+package com.getabook.domain.user.enitity;
+
+import com.getabook.domain.company.enitity.CompanyRole;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USER")
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToOne(mappedBy = "user")
+    private CompanyRole companyRole;
+}
